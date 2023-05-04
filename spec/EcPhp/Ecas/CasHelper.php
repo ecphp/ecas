b301115a388839c95198823eed729664cddf5737
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-05-04 14:20:36 +0200

    tests: cleanup

--- spec/EcPhp/Ecas/CasHelper.php
+++ spec/EcPhp/Ecas/CasHelper.php
@@ -14,16 +14,17 @@ namespace spec\EcPhp\Ecas;
 use EcPhp\CasLib\Configuration\Properties as CasProperties;
 use EcPhp\CasLib\Contract\Configuration\PropertiesInterface;
 use EcPhp\Ecas\EcasProperties;
-use Error;
 use Exception;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class CasHelper
 {
-    public static function getHttpClientMock()
+    public static function getHttpClientMock(): HttpClientInterface
     {
-        $callback = static function ($method, $url, $options) {
+        $callback = static function (string $method, string $url, array $options): ResponseInterface {
             $body = '';
             $info = [
                 'response_headers' => [
@@ -36,11 +37,6 @@ class CasHelper
                     $body = json_encode([
                         'foo' => 'bar',
                     ]);
-
-                    break;
-
-                case 'http://local/cas/login/failure/init':
-                    throw new Error('Test');
 
                     break;
 
@@ -59,56 +55,12 @@ class CasHelper
 
                 case 'http://local/cas/serviceValidate?format=JSON&service=service&ticket=ticket':
                 case 'http://local/cas/serviceValidate?service=service&ticket=ticket':
-                case 'http://local/cas/serviceValidate?ticket=ST-ticket&service=http%3A%2F%2Ffrom':
-                case 'http://local/cas/serviceValidate?ticket=ST-ticket&service=http%3A%2F%2Flocal%2Fcas%2FserviceValidate%3Fservice%3Dservice':
-                case 'http://local/cas/serviceValidate?ticket=PT-ticket&service=http%3A%2F%2Flocal%2Fcas%2FproxyValidate%3Fservice%3Dservice':
-                case 'http://local/cas/serviceValidate?ticket=PT-ticket&service=http%3A%2F%2Ffrom':
                     $body = <<< 'EOF'
                         {
                             "serviceResponse": {
                                 "authenticationSuccess": {
                                     "user": "username",
                                     "authenticationLevel": "MEDIUM"
-                                }
-                            }
-                        }
-                        EOF;
-
-                    break;
-
-                case 'http://local/cas/serviceValidate?service=service&ticket=ticket-failure':
-                    $body = <<< 'EOF'
-                        {
-                            "serviceResponse": {
-                                "authenticationFailure": {}
-                            }
-                        }
-                        EOF;
-                    $body = <<< 'EOF'
-                        <cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
-                         <cas:authenticationFailure>
-                         </cas:authenticationFailure>
-                        </cas:serviceResponse>
-                        EOF;
-
-                    break;
-
-                case 'http://local/cas/proxyValidate?service=service&ticket=ticket':
-                case 'http://local/cas/proxyValidate?ticket=PT-ticket&service=http%3A%2F%2Ffrom':
-                case 'http://local/cas/proxyValidate?ticket=ST-ticket&service=http%3A%2F%2Ffrom':
-                case 'http://local/cas/proxyValidate?service=http%3A%2F%2Flocal%2Fcas%2FproxyValidate%3Fservice%3Dservice&ticket=ticket':
-                case 'http://local/cas/proxyValidate?service=http%3A%2F%2Flocal%2Fcas%2FproxyValidate%3Fservice%3Dservice%26renew%3Dtrue&ticket=ticket&renew=true':
-                case 'http://local/cas/proxyValidate?service=http%3A%2F%2Flocal%2Fcas%2FserviceValidate%3Fservice%3Dservice&ticket=ticket':
-                case 'http://local/cas/proxyValidate?service=http%3A%2F%2Flocal%2Fcas%2FserviceValidate%3Fservice%3Dservice%26renew%3Dtrue&ticket=ticket&renew=true':
-                    $body = <<< 'EOF'
-                        {
-                            "serviceResponse": {
-                                "authenticationSuccess": {
-                                    "user": "username",
-                                    "authenticationLevel": "MEDIUM",
-                                    "proxies": [
-                                        "http://app/proxyCallback.php"
-                                    ]
                                 }
                             }
                         }
@@ -140,114 +92,6 @@ class CasHelper
                                 "authenticationSuccess": {
                                     "user": "username",
                                     "authenticationLevel": "HIGH",
-                                    "proxies": [
-                                        "http://app/proxyCallback.php"
-                                    ]
-                                }
-                            }
-                        }
-                        EOF;
-
-                    break;
-
-                case 'http://local/cas/serviceValidate?format=JSON&service=service&ticket=authenticationLevel_basic':
-                    $body = <<< 'EOF'
-                        {
-                            "serviceResponse": {
-                                "authenticationSuccess": {
-                                    "user": "username",
-                                    "authenticationLevel": "BASIC",
-                                    "proxies": [
-                                        "http://app/proxyCallback.php"
-                                    ]
-                                }
-                            }
-                        }
-                        EOF;
-
-                    break;
-
-                case 'http://local/cas/serviceValidate?ticket=ST-ticket-pgt&service=http%3A%2F%2Ffrom':
-                    $body = <<< 'EOF'
-                        {
-                            "serviceResponse": {
-                                "authenticationSuccess": {
-                                    "user": "username",
-                                    "proxyGrantingTicket": "pgtIou"
-                                }
-                            }
-                        }
-                        EOF;
-
-                    break;
-
-                case 'http://local/cas/serviceValidate?ticket=ST-ticket-pgt-pgtiou-not-found&service=http%3A%2F%2Ffrom':
-                    $body = <<< 'EOF'
-                        {
-                            "serviceResponse": {
-                                "authenticationSuccess": {
-                                    "user": "username",
-                                    "proxyGrantingTicket": "unknownPgtIou"
-                                }
-                            }
-                        }
-                        EOF;
-
-                    break;
-
-                case 'http://local/cas/proxyValidate?ticket=ST-ticket-pgt-pgtiou-pgtid-null&service=http%3A%2F%2Ffrom':
-                    $body = <<< 'EOF'
-                        {
-                            "serviceResponse": {
-                                "authenticationSuccess": {
-                                    "user": "username",
-                                    "proxyGrantingTicket": "pgtIouWithPgtIdNull"
-                                }
-                            }
-                        }
-                        EOF;
-
-                    break;
-
-                case 'http://local/cas/proxyValidate?service=service&ticket=ST-ticket-pgt':
-                case 'http://local/cas/proxyValidate?ticket=ST-ticket-pgt&service=http%3A%2F%2Ffrom':
-                case 'http://local/cas/proxyValidate?service=http%3A%2F%2Flocal%2Fcas%2FproxyValidate%3Fservice%3Dhttp%253A%252F%252Ffrom&ticket=PT-ticket-pgt':
-                    $body = <<< 'EOF'
-                        {
-                            "serviceResponse": {
-                                "authenticationSuccess": {
-                                    "user": "username",
-                                    "proxyGrantingTicket": "pgtIou",
-                                    "proxies": [
-                                        "http://app/proxyCallback.php"
-                                    ]
-                                }
-                            }
-                        }
-                        EOF;
-
-                    break;
-
-                case 'http://local/cas/serviceValidate?service=http%3A%2F%2Flocal%2Fcas%2FserviceValidate%3Fservice%3Dservice%26format%3DJSON&ticket=ticket&format=JSON':
-                    $body = <<< 'EOF'
-                        {
-                            "serviceResponse": {
-                                "authenticationSuccess": {
-                                    "user": "username"
-                                }
-                            }
-                        }
-                        EOF;
-
-                    break;
-
-                case 'http://local/cas/proxyValidate?service=http%3A%2F%2Ffrom&ticket=PT-ticket':
-                    $body = <<< 'EOF'
-                        {
-                            "serviceResponse": {
-                                "authenticationSuccess": {
-                                    "user": "username",
-                                    "proxyGrantingTicket": "pgtIou",
                                     "proxies": [
                                         "http://app/proxyCallback.php"
                                     ]
